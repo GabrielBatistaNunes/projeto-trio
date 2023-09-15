9e3934c8e93269198cb407f0d5f6d41f0b90999d
GabrielBatistaNunes <142436040+GabrielBatistaNunes@users.noreply.github.com>
2023-09-15 17:07:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,47 @@
-/* 
-function lista(titulo, descricao, stats){
-    return(`Eu sou ${titulo}, tenho ${descricao} anos, moro em ${stats} .`)
-}
-titulo = prompt("Título para sua tarefa")
-descricao = prompt("Sua idade")
-let stats = 0
-if (stats = 0) {
-stats = "pendente"    
-}
-console.log(lista(titulo, descricao, status));
-  */
-const quantidadeDePets = Number(prompt("Quantos bichinhos de estimação você tem?"));
-if (quantidadeDePets === 0) {
-  console.log("Que pena! Você pode adotar um pet!");
-} else if (quantidadeDePets > 0) {
-  const nomesDosPets = [];
-  for (let i = 1; i <= quantidadeDePets; i++) {
-    const nomePet = prompt(`Digite o nome do seu pet número ${i}:`);
-    nomesDosPets.push(nomePet);
+/* const quantidadeDeTarefas = Number(prompt("Quantas tarefas você tem?"));
+if (quantidadeDeTarefas === 0) {
+  console.log("Ótimo, sem tarefas hoje");
+} else if (quantidadeDeTarefas > 0) {
+  const nomesDasTarefas = [];
+  for (let i = 1; i <= quantidadeDeTarefas; i++) {
+    const nomeTarefa = prompt(`Digite o nome da sua ${i}ª tarefa:`);
+    nomesDasTarefas.push(nomeTarefa);
   }
-  console.log("Nomes dos seus bichinhos de estimação:");
-  console.log(nomesDosPets);
+  console.log("Nomes das suas tarefas:");
+  console.log(nomesDasTarefas);
 }
+ */
+
+ 
+function listaDeTarefas(){
+    const quantidadeDeTarefas = Number(prompt("Quantas tarefas você tem?"));
+if (quantidadeDeTarefas === 0) {
+  console.log("Ótimo, sem tarefas hoje");
+} else if (quantidadeDeTarefas > 0) {
+  const nomesDasTarefas = [];
+  //const descricao = [];
+  for (let i = 1; i <= quantidadeDeTarefas; i++) {
+    let nomeTarefa = prompt(`Digite o nome da sua ${i}ª tarefa:`);
+    nomesDasTarefas.push("Nome da tarefa: " + nomeTarefa);
+   // let textodescricao = prompt("Descrição da tarefa");
+   // descricao.push("Descrição da tarefa: " + textodescricao);
+  }
+
+  console.log(nomesDasTarefas);
+  //console.log(descricao);
+}
+    return
+
+};
+function descricaoDeTarefas() {
+    for (let i = 1; i <= listaDeTarefas(); i++){
+    const descricao = [];
+    let textodescricao = prompt("Descrição da tarefa");
+    descricao.push("Descrição da tarefa: " + textodescricao);
+}
+};
+
+
+console.log(listaDeTarefas());
+console.log(descricaoDeTarefas());
+
